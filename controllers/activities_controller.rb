4477a18614d95f695ab3ea70006084309ff7d68f
Raffaele Castaldo <raffaelecastaldo88@libero.it>
2019-10-07 14:00:19 +0100

    create/edit/delete for members and classes

--- controllers/activities_controller.rb
+++ controllers/activities_controller.rb
@@ -22,3 +22,20 @@ get '/activities/:id' do
   @activity = Activity.find(params['id'])
   erb(:"activities/show")
 end
+
+get '/activities/:id/edit' do
+  @activity = Activity.find(params['id'])
+  erb(:"activities/edit")
+end
+
+post '/activities/:id' do
+  activity = Activity.new(params)
+  activity.update
+  redirect to "/activities/#{params['id']}"
+end
+
+post '/activities/:id/delete' do
+  activity = Activity.find(params['id'])
+  activity.delete
+  redirect to '/activities'
+end
